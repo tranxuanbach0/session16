f0d4d52872b81c8f19e6ab34b5ecb1143848d591
tranxuanbach0 <trantiger706@gmail.com>
2024-12-16 13:13:04 +0700

    Add files via upload

---
+++ session16 4.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void inMang(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("Phan tu thu %d: %d\n", i, *(arr + i));
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    inMang(arr, size);
+
+    return 0;
+}
